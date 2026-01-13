15f4a3fcea8c55ee818ada0f6d19018922574674
Piotyrius <mawanwala91@gmail.com>
2026-01-13 06:19:33 +0400

    admin panel

--- src/database/prisma.module.ts
+++ src/database/prisma.module.ts
@@ -1,12 +1,14 @@
 import { Module, Global } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { SeedService } from './seed.service';
 
 @Global()
 @Module({
-  providers: [PrismaService],
+  providers: [PrismaService, SeedService],
   exports: [PrismaService],
 })
-export class PrismaModule {}
+export class PrismaModule { }
+
 
 
 
